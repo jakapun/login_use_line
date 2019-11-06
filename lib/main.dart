3acf299a7dbd3b985c1cbf95001f8d7d4bea4f60
jakapun <jakapun.c@gmail.com>
2019-11-06 10:27:11 +0700

    first

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_line_sdk/flutter_line_sdk.dart';
+// import 'package:flutter_line_sdk_example/src/screen/home_page.dart';
+
+import 'src/app.dart';
+
+void main() {
+  LineSDK.instance.setup("1620019587").then((_) {
+    print("LineSDK Prepared");
+  });
+  runApp(App());
+  // runApp(HomePage());
+}
